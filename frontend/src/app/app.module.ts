3e3d1662a56720de6be48099e07376b6d2ac7763
Wendelin Wohlgemuth <wendelinwohlgemuth@Wendelins-MacBook-Pro.local>
2023-06-19 10:21:48 -0700

    generated components for login, announcements, and select-company

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SelectCompanyComponent } from './select-company/select-company.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeAnnouncementsComponent } from './home-announcements/home-announcements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SelectCompanyComponent,
+    NavMenuComponent,
+    HomeAnnouncementsComponent
   ],
   imports: [
     BrowserModule,
