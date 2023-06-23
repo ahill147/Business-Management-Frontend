96d1c6c84f794cc016ca36210708739f747a9898
Javier Parra <82557183+JavierPHS@users.noreply.github.com>
2023-06-22 21:22:38 -0700

    Updated user service & teams component.

--- frontend/src/app/teams/teams.component.ts
+++ frontend/src/app/teams/teams.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BasicUserDto, UserService } from '../user.service';
 import TeamDto from '../interface-models/TeamDto';
 import { ModalService } from '../modal.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-teams',
@@ -12,7 +13,7 @@ export class TeamsComponent {
 
   teams: TeamDto[] = [];
 
-  constructor(public userService: UserService, protected modalService: ModalService) {}
+  constructor(public userService: UserService, protected modalService: ModalService, private router: Router) {}
 
   ngOnInit(): void {
     this.populateTeams();
@@ -28,6 +29,12 @@ export class TeamsComponent {
         console.error('Error retrieving team data:', error);
       }
     )
+  }
+
+  onTeamSelection(team: TeamDto) {
+    this.userService.team = team;
+    console.log('selection pushed');
+    this.router.navigate(['/projects']);
   }
 
   openModal(id: string) {
